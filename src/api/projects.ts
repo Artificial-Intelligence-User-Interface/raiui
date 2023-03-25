263745a5153950b27106ca0c3d94174746947708
JD Flamm <judsonwebdesign@gmail.com>
2023-03-25 04:33:15 +0000

    wrote getProjects

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,3 +1,6 @@
-export function getProjects() {
-  return [];
+import { Project } from "../types/Project";
+import { baseUrl } from "./api";
+
+export async function getProjects(): Promise<Project[]> {
+  return await fetch(baseUrl + '/aigateway/projects').then(res => res.json());
 }
